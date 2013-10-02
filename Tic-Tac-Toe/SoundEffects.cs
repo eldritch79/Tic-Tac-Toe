80276c98e884d559e2b8ef1f8423d21bd0f1ed37
Paul Pizunski <eldritch@me.com>
2013-10-02 13:08:03 +0200

    bugfix - path should be correct to "playgame.wav" now.

--- Tic-Tac-Toe/SoundEffects.cs
+++ Tic-Tac-Toe/SoundEffects.cs
@@ -10,7 +10,7 @@ class SoundEffect
         string path = System.Environment.CurrentDirectory;
         string path2 = path.Substring(0, path.LastIndexOf("bin")) + "SoundFiles" + "//playgame.wav";
 
-        using (SoundPlayer player = new SoundPlayer (@"playgame.wav"))
+       using (SoundPlayer player = new SoundPlayer (path2))
         {
             player.PlaySync();
         }
