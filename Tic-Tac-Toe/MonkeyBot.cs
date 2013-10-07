74eab247306b918fda5a037ac6ec1d625a5b099a
mrapan <kamphund@gmail.com>
2013-10-07 21:31:26 +0200

    MonkeyBot does now properly block when human has 2 in a row MonkeyBot also prioritize winning a row if it has 2 in a row MonkeyBot also says "Fuck this shit, fuck this game and fuck you!" sometimes, in debug terms "stack overflow exception"

--- Tic-Tac-Toe/MonkeyBot.cs
+++ Tic-Tac-Toe/MonkeyBot.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-// This is the computer player made by mrapan.
-// Prepare to loose!
 using System.Collections;
 using System.Collections.Generic;
+// This is the computer player made by mrapan.
+// Prepare to loose!
+using System.Security.Cryptography.X509Certificates;
 
 class MonkeyBot
 {
@@ -65,8 +66,8 @@ class MonkeyBot
 
     public static void BlockOpponentWin()
     {
-        int emptySpot = 100;
-        int k;
+        int? emptySpot = null;
+        int k = 0;
 
         int[,] winners = new int[,]
                    {
@@ -80,36 +81,83 @@ class MonkeyBot
                         {2,4,6}
                    };
 
-        // This loops thru the positions in the Winners array to check
-        // if the player who just put his mark, now has two in a row
-        // anywhere on the board.
-        for (int i = 0; i < 8; i++) // The "outer" array in Winners
+        // This checks if the bot has 2 in a row, if it does, BAM! Time to win!
+        // This is just a reversed version of the block method. Read comments down there instead!
+        for (int i = 0; i < 8; i++) 
         {
-            for (int j = 0; j < 3; j++) // The "inner" array in Winners
+            for (int j = 0; j < 3; j++)
             {
-                k = 0;
-                // If the first index in the inner array holds the players mark
-                // we do the j++ and check the next index.
-                if (Board.MarkerType == Board.ProgramBoard[winners[i, j]])
-                {
-                    k++;
+                Console.WriteLine("Nuvarande pos har:" + Board.ProgramBoard[winners[i, j]]);
 
-                }
+                if ("O" == Board.ProgramBoard[winners[i, j]]) k++;
+
+                if ("X" == Board.ProgramBoard[winners[i, j]]) k--;
+
                 else if (" " == Board.ProgramBoard[winners[i, j]])
                 {
                     emptySpot = winners[i, j];
                     Console.WriteLine("BlockPos = {0}", emptySpot);
                 }
-                // If j reaches 2, it means that we have had 2 of the players
+
+                if (k == 2)
+                {
+                    int x = j + 1;
+
+                    if (emptySpot == null && " " == Board.ProgramBoard[winners[i, x]])
+                    {
+                        PlacePiece((int)emptySpot);  
+                    }
+
+                    if (emptySpot != null && " " == Board.ProgramBoard[(int)emptySpot])
+                    {
+                        PlacePiece(winners[i, x]);
+                    }
+
+                    break;
+                }
+            }
+            k = 0;
+            emptySpot = null;
+        }
+
+        // This loops thru the positions in the Winners array to check
+        // if the player who just put his mark, now has two in a row
+        // anywhere on the board.
+        for (int i = 0; i < 8; i++) // The "outer" array in Winners
+        {
+
+            for (int j = 0; j < 3; j++) // The "inner" array in Winners
+            {
+
+                // If the first index in the inner array holds the players mark
+                // we do the j++ and check the next index.
+                Console.WriteLine("Nuvarande pos har:" + Board.ProgramBoard[winners[i, j]]);
+
+                // If winning row contains opponents mark, X, add 1 to k.
+                if ("X" == Board.ProgramBoard[winners[i, j]]) k++;
+                
+                // If winning row contains bots mark, O, delete 1 from k, 
+                // As this is not a possible winning row anymore
+                if ("O" == Board.ProgramBoard[winners[i, j]]) k--;
+
+                else if (" " == Board.ProgramBoard[winners[i, j]])
+                {
+                    emptySpot = winners[i, j];
+                    Console.WriteLine("BlockPos = {0}", emptySpot);
+                }
+                // If k reaches 2, it means that we have had 2 of the players
                 // MarkerType on a winning row, STOP HIM!
                 if (k == 2)
                 {
                     // Make a move!
-                    if (emptySpot != 100) PlacePiece(emptySpot);
+                    int x = j + 1;
+                    if (emptySpot == null) PlacePiece(winners[i, x]);
+                    if (emptySpot != null) PlacePiece((int)emptySpot);
                     break;
                 }
-
             }
+            k = 0;
+            emptySpot = null;
         }
         PlaceAtRandom();
     }
