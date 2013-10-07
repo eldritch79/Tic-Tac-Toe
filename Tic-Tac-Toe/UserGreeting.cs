53dd87801bd09fad7dc45cc66b89e0af19a5df98
mrapan <kamphund@gmail.com>
2013-10-07 23:38:16 +0200

    last trace of our console.writelines for debugging purposes, I hope

--- Tic-Tac-Toe/UserGreeting.cs
+++ Tic-Tac-Toe/UserGreeting.cs
@@ -24,7 +24,6 @@ class UserGreeting
 
         int userChoice;
         bool isNum = int.TryParse(Console.ReadLine(), out userChoice);
-        Console.WriteLine(isNum);
         // Make sure the user typed an int, if TryParse fails, this takes user back to beginning of method.
         if (!isNum) PvpOrPve();
 
